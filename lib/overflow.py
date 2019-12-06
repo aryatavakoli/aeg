896778a2103b758c41b881f4d57f32d3b0e6a948
Arya Tavakoli <aryat@outlook.com>
2019-12-05 22:27:07 -0800

    Overflow detection completed

---
+++ lib/overflow.py
@@ -0,0 +1,27 @@
+
+import angr
+import claripy
+
+def detect(simgr):
+    print(simgr)
+    if len(simgr.unconstrained):
+        for path in simgr.unconstrained:
+            state1 = path
+            eip = state1.regs.pc
+            state = state1.copy()
+            constraints = []
+            for i in range(8): #64bits/8
+                curr_byte = eip.get_byte(i)
+                constraint = claripy.And(curr_byte == 0x41)
+                constraints.append(constraint)
+            if state.satisfiable(extra_constraints=constraints):
+                for c in constraints:
+                    state.add_constraints(c)
+                if state.satisfiable():
+                    simgr.stashes['mem_corrupt'].append(path)
+                simgr.stashes['unconstrained'].remove(path)
+                simgr.drop(stash='active')
+    return simgr
+
+def exploit_overflow(binary_name):
+    return 0
